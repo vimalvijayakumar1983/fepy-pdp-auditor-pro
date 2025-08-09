428bf136f4b5717d53ba8c7cdc267efdf60377c4
vimalvijayakumar1983 <vimalvijayakumar@gmail.com>
2025-08-09 20:32:35 +0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
+
 export default config;
