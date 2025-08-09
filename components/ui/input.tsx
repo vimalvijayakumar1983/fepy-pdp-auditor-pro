831793d98d78a0516218e6826d7f947adf0300ba
vimalvijayakumar1983 <vimalvijayakumar@gmail.com>
2025-08-09 20:09:29 +0400

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,4 +1,15 @@
 import * as React from "react";
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>){
-  return <input {...props} className={`w-full border border-gray-300 px-3 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500 ${props.className||""}`} />;
-}
+
+export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+  ({ className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        {...props}
+        className={`w-full border border-gray-300 px-3 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500 ${className}`}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
